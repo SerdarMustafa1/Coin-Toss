d4a28415b298cc25e40c663b046a4153762d21e4
Serdar Mustafa <serdarmustafa@Serdar-Personal-MBP.local>
2019-07-22 20:42:01 +0300

    Updates

---
+++ precache-manifest.b3324533e7b5527db65bbe443f15faed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0175cf2b269e79461ea49a6451c4d506",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653e2afc2467c783f245",
+    "url": "/static/css/main.c9188be1.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "653e2afc2467c783f245",
+    "url": "/static/js/main.d2f1acad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
